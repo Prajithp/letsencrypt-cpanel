96944d27f093a2f6deb04a34df107ba4e9fb7df8
Keith Poole <keith@agilis.net.au>
2016-08-10 18:33:31 +0800

    Update uninstall.sh
    Changed SVG to PNG

--- uninstall.sh
+++ uninstall.sh
@@ -3,7 +3,7 @@
 rm -rvf /usr/local/cpanel/Cpanel/LetsEncrypt.pm /usr/local/cpanel/Cpanel/LetsEncrypt
 rm -rvf /usr/local/cpanel/whostmgr/docroot/cgi/letsencrypt/letsencrypt.pl
 rm -rvf /usr/local/cpanel/whostmgr/docroot/cgi/letsencrypt/index.tt
-rm -rvf /usr/local/cpanel/whostmgr/docroot/addon_plugins/ico-letsencrypt.svg
+rm -rvf /usr/local/cpanel/whostmgr/docroot/addon_plugins/ico-letsencrypt.png
 rm -rvf /scripts/renew_letsencrypt_ssl.pl
 cat /var/spool/cron/root | egrep -v "/scripts/renew_letsencrypt_ssl.pl" > /tmp/cron.tmp
 mv -f /tmp/cron.tmp /var/spool/cron/root
